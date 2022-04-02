59b702f73f6f3f133cfe755bb69d0ff2c60ac513
Brendan Robert <brendan.robert50point@gmail.com>
2022-04-02 12:11:21 -0400

    init

---
+++ src/test/kotlin/com/brobert/crud/CrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brobert.crud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
